39f58dc23a792902d18de55aea802bf991cec752
Marko Ancev <ancev.marko@yahoo.com>
2021-06-10 18:15:48 +0200

    Implement Data Classes and Entity Framework

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,13 +31,9 @@ namespace HotelListing
 
             services.AddControllers();
 
-            services.AddCors(o => {
-                o.AddPolicy("AllowAll", builder =>
-                builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());  
-            
-            });
+            services.AddDbContext<DatabaseContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+          );
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
@@ -54,9 +52,7 @@ namespace HotelListing
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HotelListing v1"));
 
-            app.UseHttpsRedirection();
-
-            app.UseCors("AllowAll");
+            app.UseHttpsRedirection();;
 
             app.UseRouting();
 
